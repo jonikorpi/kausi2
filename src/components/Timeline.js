8a328456ba2fc669ba3a6ddfca35c143a56d10e4
Joni Korpi <me@jonikorpi.com>
2017-11-24 11:11:47 +0200

    Sets today with date-fns

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,34 @@
 import React from "react";
-
+import { startOfToday, isSameDay } from "date-fns";
 // import { Link } from "react-router-dom";
 //
 // import Year from "./Year";
 // import Month from "./Month";
 // import Day from "./Day";
 
+const getToday = () => startOfToday();
+
 export default class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      today: getToday(),
+    };
+
+    this.todayRefresher = setInterval(() => {
+      const today = getToday();
+
+      if (!isSameDay(today, this.state.today)) {
+        this.setState({ today: today });
+      }
+    }, 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.todayRefresher);
+  }
+
   render() {
     return "Timeline";
   }
