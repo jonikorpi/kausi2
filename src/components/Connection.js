e0a3a39a7d181ca19e4174fd973a393218e19902
Joni Korpi <me@jonikorpi.com>
2017-11-24 12:06:54 +0200

    Adds offline RemoteStorage

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,13 +1,29 @@
 import React from "react";
+import RemoteStorage from "remotestoragejs";
 
 import Timeline from "./Timeline";
 import Navigation from "./Navigation";
 
 export default class Connection extends React.Component {
+  componentWillMount() {
+    this.remoteStorage = new RemoteStorage({
+      logging: process.env.NODE_ENV === "development",
+    });
+    this.remoteStorage.access.claim("kausi", "rw");
+  }
+
   render() {
     return [
-      <Timeline key="Timeline" {...this.props} />,
-      <Navigation key="Navigation" {...this.props} />,
+      <Timeline
+        key="Timeline"
+        {...this.props}
+        remoteStorage={this.remoteStorage}
+      />,
+      <Navigation
+        key="Navigation"
+        {...this.props}
+        remoteStorage={this.remoteStorage}
+      />,
     ];
   }
 }
