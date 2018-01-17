e5ca18cbafb3d2459f39c8a1aacb102802d1171b
Joni Korpi <me@jonikorpi.com>
2018-01-17 12:30:48 +0200

    CSS cleanup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 
 import "./reset.css";
 import "./globals.css";
+import "./components.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // registerServiceWorker();
