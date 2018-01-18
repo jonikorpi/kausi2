04ae150724477d7338a84663221d54d968fa04ff
Joni Korpi <me@jonikorpi.com>
2018-01-18 14:33:37 +0200

    Hide/show label based on editor value

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -9,19 +9,29 @@ export default class Editor extends React.Component {
     fillHeight: false,
   };
 
+  state = { value: "" };
+
+  onChange = event => {
+    this.setState({ value: event.target.value });
+    this.props.updateEntry(event.target.value);
+  };
+
   render() {
-    const { id, value, updateEntry, fillHeight } = this.props;
+    const { id, updateEntry, fillHeight } = this.props;
+    const { value } = this.state;
 
     return (
       <div className={`editor ${fillHeight ? "fillHeight" : "noFillHeight"}`}>
         <label className="label" htmlFor={id}>
-          <div className="labelText">{id}</div>
+          <div className={`labelText ${value ? "notVisible" : "visible"}`}>
+            {id}
+          </div>
           <Textarea
             id={id}
             className="textArea"
             defaultValue={value}
-            minRows={2}
-            onChange={updateEntry}
+            minRows={1}
+            onChange={this.onChange}
           />
         </label>
       </div>
