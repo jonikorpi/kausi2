5e3c67dcd4b1bcb24e83c3e238118d9c5fe8dc20
Joni Korpi <me@jonikorpi.com>
2018-08-20 13:23:02 +0300

    Prototypes properly sectioned UI

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,69 @@ import React, { Component } from "react";
 import Textarea from "react-textarea-autosize";
 
 class App extends Component {
+  state = {
+    activeEntries: "daily",
+    listOffset: 0,
+    monthlyOffset: 0,
+    dailyOffset: 0,
+  };
+
+  activateLists = () => this.setState({ activeEntries: "list" });
+  activateMonthlies = () => this.setState({ activeEntries: "monthly" });
+  activateDailies = () => this.setState({ activeEntries: "daily" });
+
   render() {
+    const {
+      activeEntries,
+      listOffset,
+      monthlyOffset,
+      dailyOffset,
+    } = this.state;
+
     return (
-      <div className="panels">
-        <div className="panel inactive">
-          {[...Array(6)].map(() => (
-            <section className="section">
-              <h1>List section</h1>
-              <Textarea />
-            </section>
-          ))}
-        </div>
+      <div className={`browser active-${activeEntries}`}>
+        <article className="timeline">
+          {[...Array(3)].map(() => (
+            <section className="month">
+              <div className="lists-track">
+                <div className="entry list placeholder" />
+              </div>
+              <div className="month-track">
+                <section className="entry monthly">
+                  <h1>Monthly</h1>
+                  <Textarea
+                    onFocus={this.activateMonthlies}
+                    defaultValue="Monthly entry"
+                  />
+                </section>
+              </div>
 
-        <div className="panel inactive">
-          {[...Array(2)].map(() => (
-            <section className="section">
-              <h1>Month section</h1>
-              <Textarea />
+              <section className="dailies-track">
+                {[...Array(30)].map(() => (
+                  <section className="entry daily">
+                    <h1>Daily</h1>
+                    <Textarea
+                      onFocus={this.activateDailies}
+                      defaultValue="Daily entry"
+                    />
+                  </section>
+                ))}
+              </section>
             </section>
           ))}
-        </div>
+        </article>
 
-        <div className="panel active">
-          {[...Array(60)].map(() => (
-            <section className="section">
-              <h1>Day section</h1>
-              <Textarea />
+        <article className="lists">
+          {[...Array(50)].map(() => (
+            <section className="entry list">
+              <h1>List</h1>
+              <Textarea
+                onFocus={this.activateLists}
+                defaultValue="List entry"
+              />
             </section>
           ))}
-        </div>
+        </article>
       </div>
     );
   }
