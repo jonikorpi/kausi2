8693df6270f25c76773ef5f3e69a63feb926003c
Joni Korpi <me@jonikorpi.com>
2018-08-21 11:41:36 +0300

    Sets up scrolling panels

--- src/components/App.js
+++ src/components/App.js
@@ -1,142 +1,139 @@
 import React, { Component } from "react";
 import Textarea from "react-textarea-autosize";
+import { startOfToday, addDays, format } from "date-fns";
 
-const scrollIntoView = (element, offset) => {
-  const { top, height } = element.getBoundingClientRect();
-  const { clientHeight } = document.documentElement;
-  const isAbove = top < 0;
-  const isBelow = top + height > clientHeight;
+// const scrollIntoView = (element, offset) => {
+//   const { top, height } = element.getBoundingClientRect();
+//   const { clientHeight } = document.documentElement;
+//   const isAbove = top < 0;
+//   const isBelow = top + height > clientHeight;
 
-  if (isAbove || isBelow) {
-    window.scrollTo(0, window.scrollY + top - offset);
-  }
-};
+//   if (isAbove || isBelow) {
+//     window.scrollTo(0, window.scrollY + top - offset);
+//   }
+// };
 
 class App extends Component {
   state = {
-    activeEntries: "dailies",
-    listsOffset: 0,
-    monthliesOffset: 0,
-    dailiesOffset: 0,
+    activePanel: "calendar",
+    calendarScrolledTo: 0,
+    listsScrolledTo: 0,
   };
 
-  activateLists = event => {
-    event.persist();
-    const element = event.nativeEvent.target.parentNode;
-    const offset = element.getBoundingClientRect().top;
-    const activeOffset = `${this.state.activeEntries}Offset`;
+  activateCalendar = () => {
+    const scrollTo = this.state.calendarScrolledTo;
     this.setState(
       {
-        activeEntries: "lists",
-        [activeOffset]: this.state[activeOffset] || -window.scrollY,
-        listsOffset: 0,
+        activePanel: "calendar",
+        calendarScrolledTo: 0,
+        listsScrolledTo: window.scrollY,
       },
-      () => scrollIntoView(element, offset)
+      () => {
+        window.scrollTo(0, scrollTo);
+        this.lists.scrollTop = this.state.listsScrolledTo;
+      }
     );
   };
-  activateMonthlies = event => {
-    event.persist();
-    const element = event.nativeEvent.target.parentNode;
-    const offset = element.getBoundingClientRect().top;
-    const activeOffset = `${this.state.activeEntries}Offset`;
+  activateLists = () => {
+    const scrollTo = this.state.listsScrolledTo;
     this.setState(
       {
-        activeEntries: "monthlies",
-        [activeOffset]: this.state[activeOffset] || -window.scrollY,
-        monthliesOffset: 0,
+        activePanel: "lists",
+        listsScrolledTo: 0,
+        calendarScrolledTo: window.scrollY,
       },
-      () => scrollIntoView(element, offset)
-    );
-  };
-  activateDailies = event => {
-    event.persist();
-    const element = event.nativeEvent.target.parentNode;
-    const offset = element.getBoundingClientRect().top;
-    const activeOffset = `${this.state.activeEntries}Offset`;
-    this.setState(
-      {
-        activeEntries: "dailies",
-        [activeOffset]: this.state[activeOffset] || -window.scrollY,
-        dailiesOffset: 0,
-      },
-      () => scrollIntoView(element, offset)
+      () => {
+        window.scrollTo(0, scrollTo);
+        this.calendar.scrollTop = this.state.calendarScrolledTo;
+      }
     );
   };
 
   render() {
-    const {
-      activeEntries,
-      listsOffset,
-      monthliesOffset,
-      dailiesOffset,
-    } = this.state;
+    const { activePanel, calendarScrolledTo, listsScrolledTo } = this.state;
 
     return (
-      <div
-        className={`browser active-${activeEntries}`}
-        style={{
-          "--listsOffset": listsOffset + "px",
-          "--monthliesOffset": monthliesOffset + "px",
-          "--dailiesOffset": dailiesOffset + "px",
-        }}
-      >
-        <article className="timeline">
+      <React.Fragment>
+        <article
+          ref={element => (this.calendar = element)}
+          className={`panel calendar ${
+            activePanel === "calendar" ? "active" : "inactive"
+          }`}
+        >
           {[...Array(3)].map((value, index) => (
             <section className="month" key={index}>
-              <div className="lists-track track placeholder">
-                <div className="entry list" />
-              </div>
-              <div className="monthlies-track track">
+              <h1>Month {index + 1}</h1>
+              <div className="grid">
                 <section className="entry monthly">
-                  <h1>Month {index + 1}</h1>
-                  <Textarea
-                    onFocus={this.activateMonthlies}
-                    defaultValue={[...Array(Math.pow(55, index + 2) % 24 + 1)]
-                      .map(() => `Monthly entry ${index + 1}`)
-                      .join("\n")}
-                  />
+                  <div className="entry-body">
+                    <Textarea
+                      onFocus={this.activateMonthlies}
+                      defaultValue={[...Array(Math.pow(55, index + 2) % 24 + 1)]
+                        .map(() => `Monthly entry ${index + 1}`)
+                        .join("\n")}
+                    />
+                  </div>
                 </section>
               </div>
 
-              <section className="dailies-track track">
+              <div className="grid">
                 {[...Array(30)].map((value, index) => (
                   <section className="entry daily" key={index}>
-                    <h1>Daily {index + 1}</h1>
-                    <Textarea
-                      onFocus={this.activateDailies}
-                      defaultValue={[...Array(Math.pow(55, index + 2) % 24 + 1)]
-                        .map(() => `Daily entry ${index + 1}`)
-                        .join("\n")}
-                    />
+                    <div className="entry-body">
+                      <h2>
+                        {format(addDays(startOfToday(), index), "DD dddd")}
+                      </h2>
+                      <Textarea
+                        onFocus={this.activateDailies}
+                        defaultValue={[
+                          ...Array(Math.pow(55, index + 2) % 24 + 1),
+                        ]
+                          .map(() => `Daily entry ${index + 1}`)
+                          .join("\n")}
+                      />
+                    </div>
                   </section>
                 ))}
-              </section>
+              </div>
             </section>
           ))}
         </article>
 
-        <article className="lists">
-          <div className="lists-track track">
+        <article
+          ref={element => (this.lists = element)}
+          className={`panel lists ${
+            activePanel === "lists" ? "active" : "inactive"
+          }`}
+        >
+          <h1>Lists</h1>
+          <div className="grid">
             {[...Array(50)].map((value, index) => (
               <section className="entry list" key={index}>
-                <h1>List {index}</h1>
-                <Textarea
-                  onFocus={this.activateLists}
-                  defaultValue={[...Array(Math.pow(55, index + 2) % 24 + 1)]
-                    .map(() => `List entry ${index + 1}`)
-                    .join("\n")}
-                />
+                <div className="entry-body">
+                  <h2>List {index}</h2>
+                  <Textarea
+                    onFocus={this.activateLists}
+                    defaultValue={[...Array(Math.pow(55, index + 2) % 24 + 1)]
+                      .map(() => `List entry ${index + 1}`)
+                      .join("\n")}
+                  />
+                </div>
               </section>
             ))}
           </div>
-          <div className="monthlies-track track placeholder">
-            <div className="entry monthly" />
-          </div>
-          <div className="dailies-track track placeholder">
-            <div className="entry daily" />
-          </div>
         </article>
-      </div>
+
+        <button
+          type="button"
+          className={`clicker ${activePanel === "calendar" ? "top" : "bottom"}`}
+          title={`Expand ${activePanel === "calendar" ? "lists" : "calendar"}`}
+          onClick={
+            activePanel === "calendar"
+              ? this.activateLists
+              : this.activateCalendar
+          }
+        />
+      </React.Fragment>
     );
   }
 }
