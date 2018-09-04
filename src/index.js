9e4e0d1539bee824fdbe9572d34b3f5e5479b07c
Joni Korpi <me@jonikorpi.com>
2018-09-04 13:33:12 +0300

    Disables serviceworker for now

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 import "jonikorpi-base-files/reset.css";
 import "./stylesheets/main.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
