b8dd19444abf7909f622a53ed1d0209762a3d09c
Joni Korpi <me@jonikorpi.com>
2018-09-06 15:53:03 +0300

    Adds month navigation

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,11 @@ import {
   getDaysInMonth,
   getISOWeek,
   differenceInMilliseconds,
-  differenceInMonths,
-  subMonths,
-  addMonths,
   isSameDay,
   isSameMonth,
   isWeekend,
+  setYear,
+  setMonth,
 } from "date-fns/esm";
 
 import Data from "../components/Data";
@@ -36,6 +35,37 @@ const pathsFromDay = day => ({
     "dd"
   )}.txt`,
 });
+
+const isMonthInputSupported = () => {
+  const monthInputTest = document.createElement("input");
+  monthInputTest.type = "month";
+  return monthInputTest.type === "text" ? false : true;
+};
+const monthInputSupported = isMonthInputSupported();
+
+const getYearOptions = date => {
+  const centerYear = +format(date, "YYYY");
+  const years = [];
+  for (let year = centerYear - 10; year <= centerYear + 10; year++) {
+    years.push(year);
+  }
+  return years.map(year => (
+    <option value={year} key={year}>
+      {year}
+    </option>
+  ));
+};
+const getMonthOptions = date => {
+  const months = [];
+  for (let month = 1; month <= 12; month++) {
+    months.push(month);
+  }
+  return months.map(month => (
+    <option value={month} key={month}>
+      {format(setMonth(date, month - 1), "MMMM")}
+    </option>
+  ));
+};
 
 class App extends Component {
   state = { databaseReady: false };
@@ -74,8 +104,7 @@ class App extends Component {
 class Calendar extends Component {
   state = {
     today: startOfToday(),
-    startAtMonth: startOfMonth(subMonths(Date.now(), 0)),
-    endAtMonth: startOfMonth(addMonths(Date.now(), 0)),
+    month: startOfMonth(startOfToday()),
   };
 
   componentDidMount() {
@@ -98,90 +127,128 @@ class Calendar extends Component {
     this.setTodayTimer();
   };
 
-  render() {
-    const { today, startAtMonth, endAtMonth } = this.state;
+  changeMonthAndYear = event => {
+    const [year, month] = event.target.value.split("-");
 
-    const months = [
-      ...Array(differenceInMonths(endAtMonth, startAtMonth) + 1),
-    ].map((value, index) => addMonths(startAtMonth, index));
+    if (month && year) {
+      this.setState({
+        month: setMonth(
+          setYear(startOfMonth(this.state.month), year),
+          month - 1
+        ),
+      });
+    }
+  };
+
+  changeMonth = event =>
+    this.setState({
+      month: setMonth(startOfMonth(this.state.month), event.target.value - 1),
+    });
+  changeYear = event =>
+    this.setState({
+      month: setYear(startOfMonth(this.state.month), event.target.value),
+    });
+
+  render() {
+    const { today, month } = this.state;
+
+    const days = [...Array(getDaysInMonth(month))].map((value, index) =>
+      addDays(month, index)
+    );
+
+    const weeks = Object.values(
+      days.reduce((weeks, day) => {
+        const week = getISOWeek(day);
+        weeks[week] = weeks[week] || [];
+        weeks[week].push(day);
+        return weeks;
+      }, {})
+    );
+
+    const firstWeek = weeks[0];
+    const lastWeek = weeks[weeks.length - 1];
+
+    if (firstWeek.length < 7) {
+      const daysToAdd = firstWeek.length;
+      for (let index = 0; index < 7 - daysToAdd; index++) {
+        firstWeek.unshift(subDays(firstWeek[0], 1));
+      }
+    }
+
+    if (lastWeek.length < 7) {
+      const daysToAdd = lastWeek.length;
+      for (let index = 0; index < 7 - daysToAdd; index++) {
+        lastWeek.push(addDays(lastWeek[lastWeek.length - 1], 1));
+      }
+    }
 
     return (
-      <Fragment>
-        {months.map(month => {
-          const days = [...Array(getDaysInMonth(month))].map((value, index) =>
-            addDays(month, index)
-          );
+      <section className="section month">
+        <div className="section-title">
+          {monthInputSupported ? (
+            <input
+              type="month"
+              defaultValue={format(month, "YYYY-MM")}
+              className="selector month-and-year-selector"
+              onChange={this.changeMonthAndYear}
+            />
+          ) : (
+            <Fragment>
+              <div className="selector month-selector">
+                <select onChange={this.changeMonth} value={format(month, "M")}>
+                  {getMonthOptions(month)}
+                </select>
+              </div>
+              <div className="selector year-selector">
+                <select
+                  onChange={this.changeYear}
+                  value={format(month, "YYYY")}
+                >
+                  {getYearOptions(month)}
+                </select>
+              </div>
+            </Fragment>
+          )}
+        </div>
 
-          const weeks = Object.values(
-            days.reduce((weeks, day) => {
-              const week = getISOWeek(day);
-              weeks[week] = weeks[week] || [];
-              weeks[week].push(day);
-              return weeks;
-            }, {})
-          );
-
-          const firstWeek = weeks[0];
-          const lastWeek = weeks[weeks.length - 1];
-
-          if (firstWeek.length < 7) {
-            const daysToAdd = firstWeek.length;
-            for (let index = 0; index < 7 - daysToAdd; index++) {
-              firstWeek.unshift(subDays(firstWeek[0], 1));
-            }
-          }
-
-          if (lastWeek.length < 7) {
-            const daysToAdd = lastWeek.length;
-            for (let index = 0; index < 7 - daysToAdd; index++) {
-              lastWeek.push(addDays(lastWeek[lastWeek.length - 1], 1));
-            }
-          }
-
-          return (
-            <section className="section month" key={format(month, "MM-YYYY")}>
-              <h1 className="section-title">{format(month, "MMMM MM/YYYY")}</h1>
-              {Object.values(weeks).map((week, index) => (
-                <div key={index} className="grid">
-                  {week.map(day => {
-                    const key = format(day, "dd-MM-YYYY");
-                    return (
-                      <Entry
-                        key={key}
-                        classNames={{
-                          weekend: isWeekend(day),
-                          ethereal: !isSameMonth(month, day),
-                          isToday: isSameDay(day, today),
-                          hasTitle: !!day,
-                        }}
-                      >
-                        <DayData paths={pathsFromDay(day)}>
-                          {(value, update) => {
-                            return (
-                              <div className="editor">
-                                <label htmlFor={key} className="editor-title">
-                                  {format(day, "dd EEE")}
-                                </label>
-                                <Textarea
-                                  className="textarea"
-                                  id={key}
-                                  value={value}
-                                  onChange={update}
-                                  minRows={3}
-                                />
-                              </div>
-                            );
-                          }}
-                        </DayData>
-                      </Entry>
-                    );
-                  })}
-                </div>
-              ))}
-            </section>
-          );
-        })}
-      </Fragment>
+        {Object.values(weeks).map((week, index) => (
+          <div key={index} className="grid">
+            {week.map(day => {
+              const key = format(day, "dd-MM-YYYY");
+              return (
+                <Entry
+                  key={key}
+                  classNames={{
+                    weekend: isWeekend(day),
+                    ethereal: !isSameMonth(month, day),
+                    isToday: isSameDay(day, today),
+                    hasTitle: !!day,
+                  }}
+                >
+                  <DayData paths={pathsFromDay(day)}>
+                    {(value, update) => {
+                      return (
+                        <div className="editor">
+                          <label htmlFor={key} className="editor-title">
+                            {format(day, "dd EEE")}
+                          </label>
+                          <Textarea
+                            className="textarea"
+                            id={key}
+                            value={value}
+                            onChange={update}
+                            minRows={3}
+                          />
+                        </div>
+                      );
+                    }}
+                  </DayData>
+                </Entry>
+              );
+            })}
+          </div>
+        ))}
+      </section>
     );
   }
 }
