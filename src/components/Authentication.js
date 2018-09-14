aad9cae4c94c4ea4a8da535c2add0e358cd3aa21
Joni Korpi <me@jonikorpi.com>
2018-09-14 11:23:59 +0300

    Disables FB and Twitter auths

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -12,8 +12,8 @@ const uiConfig = {
   // autoUpgradeAnonymousUsers: true,
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     {
       provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
